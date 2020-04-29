6785d3c9c8a347b1a9ed8ac61eeda86576fbc209
TheMshary <themshary94@gmail.com>
2020-04-29 17:56:59 +0300

    Review notes

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,7 +4,8 @@ import { combineReducers } from "redux";
 
 import imageReducer from "./image";
 import returnedImageReducer from "./returnedImage";
-
+// You can combine these reducers into images.js
+// no need to split
 const rootReducer = combineReducers({
   image: imageReducer,
   returnedImage: returnedImageReducer,
